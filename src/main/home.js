3468224bc713a2ade4b104f6a625cb3eb08c141a
leena72 <leena.sharma72@gmail.com>
2020-07-20 20:59:09 +0530

    home style and router

---
+++ src/main/home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        // <div className="home">
+        //     <img src="./shopping.jpg"></img>
+        //     <span>
+        //         Shopping {"\n"}is{"\n"} Always{"\n"} A{"\n"}Good{"\n"}Idea!!!
+        //     </span>
+        // </div>
+
+        <div className="container">
+
+            <img src="./shopping.jpg" className="image" ></img>
+            <span>
+                Shopping {"\n"}is{"\n"} Always{"\n"} A{"\n"}Good{"\n"}Idea!!!
+            </span>
+            <div className="middle">
+                <div className="text">Lets Shop</div>
+            </div>
+        </div>
+    )
+}
