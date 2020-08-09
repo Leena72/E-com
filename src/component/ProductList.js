e6cf6f3a2305f47cc0b48f61d38285cabeac451b
leena72 <leena.sharma72@gmail.com>
2020-08-09 19:52:49 +0530

    format currency

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
+import formatCurrency from "../util"
 
 class ProductList extends Component {
     render() {
         return (
-            <ul>
+            <ul className="product">
                 {
                     this.props.product.map((product) => {
                         return (
                             <li key={product._id}>
-                                <div className="product">
+                                <div className="dress">
                                     <a href={"#"+product._id}>
                                         <img src={product.image} alt={product.title}></img>
                                         <p>{product.title}</p>
                                     </a>
+                                </div>    
                                     <div className="productPrice">
-                                        <div>{product.price}</div>
+                                        <div className="price">{formatCurrency(product.price)}</div>
+                                        {/* <div className="price">{product.price}</div> */}
                                         <button className="bn">ADD TO CART</button>
                                     </div>
-                                </div>
+                                
 
                             </li>
                         )
